508e5db81993a33940aef290ffd20e62aea2fa23
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-06-07 17:24:27 +0200

    set variables for account card component

--- src/Components/AccountCard/index.jsx
+++ src/Components/AccountCard/index.jsx
@@ -1,14 +1,14 @@
 import './account.css'
 
 function AccountCard(account) {
-
+    const config = account.account
 
     return (
         <section className="account">
             <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
+            <h3 className="account-title">{config.title}</h3>
+            <p className="account-amount">{config.amount}</p>
+            <p className="account-amount-description">{config.description}</p>
             </div>
             <div className="account-content-wrapper cta">
             <button className="transaction-button">View transactions</button>
