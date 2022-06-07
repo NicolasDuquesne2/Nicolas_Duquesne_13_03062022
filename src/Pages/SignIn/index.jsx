29b417b1f1dad05ee4c55261adc2f19d09c67af5
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-06-07 16:37:38 +0200

    Add non functional sign in page

---
+++ src/Pages/SignIn/index.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import Header from "../../Components/Header"
+import Footer from "../../Components/Footer"
+import './signin.css'
+
+function SignIn() {
+    return (
+        <React.Fragment>
+            <Header />
+            <main className="main bg-dark">
+            <section className="sign-in-content">
+                    <FontAwesomeIcon icon={faCircleUser} />
+                    <h1>Sign In</h1>
+                    <form>
+                    <div className="input-wrapper">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" />
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+                        <a href="./user.html" className="sign-in-button">Sign In</a>
+                    </form>
+                </section>
+            </main>
+            <Footer />
+        </React.Fragment>
+    )
+}
+
+export default SignIn
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import Home from './Pages/Home';
 import './main.css'
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -12,6 +13,7 @@ root.render(
   <Router >
       <Routes >
         <Route exact path="/" element={ <Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
   </Router>
 );
